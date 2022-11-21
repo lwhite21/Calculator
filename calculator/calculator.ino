be3e46f68b02c5a630bbb1f3beda1843750ebd0c
Logan White <lwhite21@huskers.unl.edu>
2022-11-21 15:36:18 -0600

    Can now enter numbers and choose your operator, result will only print to serial.

--- calculator/calculator.ino
+++ calculator/calculator.ino
@@ -48,10 +48,11 @@ void handle_buttonpress(void) {
   unsigned long now = millis();
   if (now - last_button_press > DEBOUNCE_TIME) {
     last_button_press = now;
-    if (cowpi_left_button_is_pressed) {
+    if (cowpi_left_button_is_pressed()) {
       Serial.println("Left button was pressed");
-    } else  {
+    } else if (cowpi_right_button_is_pressed()) {
       Serial.println("Right button was pressed");
+      clear_display();
     }
   }
 }
@@ -62,6 +63,73 @@ void handle_keypress(void) {
   unsigned long now = millis();
   if (now - last_key_press > DEBOUNCE_TIME) {
     last_key_press = now;
-    Serial.println(cowpi_get_keypress());
+    display_keypress(cowpi_get_keypress());
+  }
+}
+
+static char operator_1[10] = "";
+static char operator_2[10] = "";
+static int operation = 0;
+void display_keypress(char key) {
+  if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4'
+   || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
+    if (cowpi_left_switch_in_left_position()) {
+      int length = strlen(operator_1);
+      if (length < 8) {
+        operator_1[length] = key;
+      }
+    } else {
+      int length = strlen(operator_2);
+      if (length < 8) {
+        operator_2[length] = key;
+      }
+    }
+  } else if (key == 'A') {
+    operation = 1;
+    cowpi_lcd1602_place_character(0x40, '+');
+  } else if (key == 'B') {
+    operation = 2;
+    cowpi_lcd1602_place_character(0x40, '-');
+  } else if (key == 'C') {
+    operation = 3;
+    cowpi_lcd1602_place_character(0x40, 'x');
+  } else if (key == 'D') {
+    operation = 4;
+    cowpi_lcd1602_place_character(0x40, 0xFD);
+  } else if (key == '#') {
+    if (operation == 1) {
+      Serial.println(atoi(operator_1) + atoi(operator_2));
+    } else if (operation == 2) {
+      Serial.println(atoi(operator_1) - atoi(operator_2));
+    } else if (operation == 3) {
+      Serial.println(atoi(operator_1) * atoi(operator_2));
+    } else if (operation == 4) {
+      Serial.println(atoi(operator_1) / atoi(operator_2));
+    } 
+  }
+
+  // display operands
+  int i = strlen(operator_1);
+  int j = strlen(operator_2);
+  const char *c = operator_1;
+  const char *s = operator_2;
+  while (*c != '\0') {
+    cowpi_lcd1602_place_character(0x10 - i, *c++);
+    i--;
+  }
+  while (*s != '\0') {
+    cowpi_lcd1602_place_character(0x50 - j, *s++);
+    j--;
+  }
+}
+
+void clear_display(void) {
+  memset(operator_1,0,sizeof(operator_1));
+  memset(operator_2,0,sizeof(operator_2));
+  int index = 0;
+  while (index < 16) {   
+    cowpi_lcd1602_place_character(0x00 + index, ' ');
+    cowpi_lcd1602_place_character(0x00 + index + 0x40 , ' ');
+    index++;
   }
 }
