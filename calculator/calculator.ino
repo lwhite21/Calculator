b98a8fe69c51cc9dfe6cfd03ec5a147b557284cb
Logan White <lwhite21@huskers.unl.edu>
2022-11-22 11:40:35 -0600

    Accounted for most edge cases

--- calculator/calculator.ino
+++ calculator/calculator.ino
@@ -35,6 +35,7 @@ void setup() {
   timer_interrupt_masks[0] |= 0x2;
   attachInterrupt(digitalPinToInterrupt(2), handle_buttonpress, HIGH);
   attachInterrupt(digitalPinToInterrupt(3), handle_keypress, CHANGE);
+  cowpi_lcd1602_place_character(0xF, '0');
 }
 
 void loop() {
@@ -50,6 +51,7 @@ void handle_buttonpress(void) {
     last_button_press = now;
     if (cowpi_left_button_is_pressed()) {
       Serial.println("Left button was pressed");
+      negate_operator();
     } else if (cowpi_right_button_is_pressed()) {
       Serial.println("Right button was pressed");
       clear_display();
@@ -70,49 +72,94 @@ void handle_keypress(void) {
 static char operator_1[10] = "";
 static char operator_2[10] = "";
 static int operation = 0;
+static int operator_1_negative = 1;
+static int operator_2_negative = 1;
+static int one_or_two = 1;
 void display_keypress(char key) {
   if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4'
    || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
-    if (cowpi_left_switch_in_left_position()) {
-      int length = strlen(operator_1);
-      if (length < 8) {
-        operator_1[length] = key;
-      }
-    } else {
-      int length = strlen(operator_2);
-      if (length < 8) {
-        operator_2[length] = key;
-      }
+    int length = strlen(operator_2);
+    if (length < 9) {
+      operator_2[length] = key;
     }
-  } else if (key == 'A') {
-    operation = 1;
-    cowpi_lcd1602_place_character(0x40, '+');
-  } else if (key == 'B') {
-    operation = 2;
-    cowpi_lcd1602_place_character(0x40, '-');
-  } else if (key == 'C') {
-    operation = 3;
-    cowpi_lcd1602_place_character(0x40, 'x');
-  } else if (key == 'D') {
-    operation = 4;
-    cowpi_lcd1602_place_character(0x40, 0xFD);
-  } else if (key == '#') {
-    if (operation == 1) {
-      Serial.println(atoi(operator_1) + atoi(operator_2));
-    } else if (operation == 2) {
-      Serial.println(atoi(operator_1) - atoi(operator_2));
-    } else if (operation == 3) {
-      Serial.println(atoi(operator_1) * atoi(operator_2));
-    } else if (operation == 4) {
-      Serial.println(atoi(operator_1) / atoi(operator_2));
+  } else if (key == 'A' || key == 'B' || key == 'C' || key == 'D') {
+    blank_display();
+    operator_1_negative = operator_2_negative;
+    operator_2_negative = 1;
+    one_or_two = 2;
+    memset(operator_1,0,sizeof(operator_1));
+    strcpy(operator_1, operator_2);
+    if (strlen(operator_1) == 0) {
+      operator_1[0] = '0';
+    }
+    memset(operator_2,0,sizeof(operator_2));
+    if (key == 'A') {
+      operation = 1;
+      cowpi_lcd1602_place_character(0x40, '+');
+    } else if (key == 'B') {
+      operation = 2;
+      cowpi_lcd1602_place_character(0x40, '-');
+    } else if (key == 'C') {
+      operation = 3;
+      cowpi_lcd1602_place_character(0x40, 'x');
+    } else if (key == 'D') {
+      operation = 4;
+      cowpi_lcd1602_place_character(0x40, 0xFD);
     } 
+  } else if (key == '#') {
+    if (one_or_two == 1) {
+      blank_display();
+      strcpy(operator_1, operator_2);
+      operator_1_negative = operator_2_negative;
+      operator_2_negative = 1;
+      memset(operator_2,0,sizeof(operator_2));
+      
+    } else {
+      one_or_two = 1;
+      long result;
+      if (operation == 1) {
+        result = ((long) strtol(operator_1, (char **)NULL, 10))*operator_1_negative + ((long) strtol(operator_2, (char **)NULL, 10))*operator_2_negative;
+      } else if (operation == 2) {
+        result = ((long) strtol(operator_1, (char **)NULL, 10))*operator_1_negative - ((long) strtol(operator_2, (char **)NULL, 10))*operator_2_negative;
+      } else if (operation == 3) {
+        result = ((long) strtol(operator_1, (char **)NULL, 10))*operator_1_negative * ((long) strtol(operator_2, (char **)NULL, 10))*operator_2_negative;
+      } else if (operation == 4) {
+        result = ((long) strtol(operator_1, (char **)NULL, 10))*operator_1_negative / ((long) strtol(operator_2, (char **)NULL, 10))*operator_2_negative;
+      } 
+      clear_display();
+      if (result < 0) {
+        result = result * -1;
+        operator_1_negative = -1;
+      } else {
+        operator_1_negative = 1;
+      }
+      itoa(result, operator_1, 10);
+      operation = 0;
+    }
   }
 
-  // display operands
+  display_operands();
+}
+
+void display_operands(void) {
   int i = strlen(operator_1);
   int j = strlen(operator_2);
   const char *c = operator_1;
   const char *s = operator_2;
+  if (strlen(operator_1) > 0) {
+    if (operator_1_negative == -1) {
+      cowpi_lcd1602_place_character(0x10 - i - 1, '-');
+    } else {
+      cowpi_lcd1602_place_character(0x10 - i - 1, ' ');
+    }
+  }
+  if (strlen(operator_2) > 0) {
+    if (operator_2_negative == -1) {
+      cowpi_lcd1602_place_character(0x50 - j - 1, '-');
+    } else {
+      cowpi_lcd1602_place_character(0x50 - j - 1, ' ');
+    } 
+  }
   while (*c != '\0') {
     cowpi_lcd1602_place_character(0x10 - i, *c++);
     i--;
@@ -123,9 +170,30 @@ void display_keypress(char key) {
   }
 }
 
+void negate_operator() {
+  if (one_or_two = 1) {
+    operator_2_negative = operator_2_negative * -1;
+  } else {
+    operator_1_negative = operator_1_negative * -1;
+  }
+  display_operands();
+}
+
 void clear_display(void) {
   memset(operator_1,0,sizeof(operator_1));
   memset(operator_2,0,sizeof(operator_2));
+  operator_1_negative = 1;
+  operator_2_negative = 1;
+  int index = 0;
+  while (index < 16) {   
+    cowpi_lcd1602_place_character(0x00 + index, ' ');
+    cowpi_lcd1602_place_character(0x00 + index + 0x40 , ' ');
+    index++;
+  }
+  cowpi_lcd1602_place_character(0xF, '0');
+}
+
+void blank_display(void) {
   int index = 0;
   while (index < 16) {   
     cowpi_lcd1602_place_character(0x00 + index, ' ');
